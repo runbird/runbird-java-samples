f9c40626635874cf9aa88550b686f90c2c2f0d01
suocaiyuan <suocaiyuan@mathartsys.com>
2018-07-10 00:23:27 +0800

    add method of java8 for learn

---
+++ src/main/java/com/scy/java8/lambda/DefaultInterfaceDemo.java
@@ -0,0 +1,12 @@
+package com.scy.java8.lambda;
+
+/**
+ * 类名： DefaultInterfaceDemo <br>
+ * 描述：TODO <br>
+ * 创建日期： 2018/7/10 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class DefaultInterfaceDemo {
+}
---
+++ src/main/java/com/scy/java8/lambda/PredicateDemo.java
@@ -0,0 +1,12 @@
+package com.scy.java8.lambda;
+
+/**
+ * 类名： PredicateDemo <br>
+ * 描述：TODO <br>
+ * 创建日期： 2018/7/9 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class PredicateDemo {
+}
