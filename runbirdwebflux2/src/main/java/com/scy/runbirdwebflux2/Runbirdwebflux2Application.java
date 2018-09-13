1d8907320ea71fa2edab5c077e29be133868c9f6
suocaiyuan <suocaiyuan@mathartsys.com>
2018-09-13 23:49:13 +0800

    add project runbirdwebflux2

---
+++ runbirdwebflux2/src/main/java/com/scy/runbirdwebflux2/Runbirdwebflux2Application.java
@@ -0,0 +1,12 @@
+package com.scy.runbirdwebflux2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runbirdwebflux2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runbirdwebflux2Application.class, args);
+    }
+}
---
+++ runbirdwebflux2/src/main/java/com/scy/runbirdwebflux2/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.scy.runbirdwebflux2.repository;
+
+import com.scy.runbirdwebflux2.domain.User;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+/**
+ * 类名： UserRepository <br>
+ * 描述：TODO <br>
+ * 创建日期： 2018/9/5 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+
+@Repository(value = "userRepository")
+public interface UserRepository extends ReactiveMongoRepository<User,String> {
+
+    Flux<User> findByAgeBetween(int lower, int higer);
+
+    @Query(value = "{'age':{'$gte':0,'$lte':120}}")
+    Flux<User> olderUser();
+}
