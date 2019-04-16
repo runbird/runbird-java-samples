b7f4b5bccbb0c5a0decb79c0356f7bd140a8be2f
suocaiyuan <suocaiyuan@163.com>
2019-04-16 10:57:58 +0800

    add module webflux-client

---
+++ runbirdwebfluxclient/src/main/java/com/scy/RunbirdwebfluxclientApplication.java
@@ -0,0 +1,12 @@
+package com.scy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunbirdwebfluxclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunbirdwebfluxclientApplication.class, args);
+    }
+}
---
+++ runbirdwebfluxclient/src/test/java/com/scy/RunbirdwebfluxclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunbirdwebfluxclientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
